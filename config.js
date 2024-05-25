7f474e0e14b52ebfe64f8a7aa21ed478a1175623
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-05-25 11:38:33 +0300

    𝗩𝗜𝗥𝗨𝗦𝗜-𝗠𝗕𝗔𝗬𝗔-𝗠𝗗

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "2547xxxx";
 
 global.api_smd = "https://api-smd.onrender.com";
-global.scan = "https://web.giftedtechnexus.co.ke";
+global.scan = "https://virusi-2615bdac6a00.herokuapp.com/pair";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
